51d1ac723b62f19d0ebfea14c5c1aa9ad237787c
anohin <conceptcontent@gmail.com>
2017-03-26 21:19:10 +0500

    Создание конструктора класса Point

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -13,16 +13,10 @@ namespace snake
         static void Main(string[] args)
         {
 
-            Point p1 = new snake.Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new snake.Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new snake.Point();
-            p2.x = 4;
-            p2.y = 6;
-            p2.sym = '#';
+            Point p2 = new snake.Point(4, 6, '#');
             p2.Draw();
 
             Console.ReadLine();
