d4250f188f699b3b2b95f19ddffb29a8e3d8761b
anohin <conceptcontent@gmail.com>
2017-03-26 21:04:28 +0500

    Создание первого класса Point.

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -13,24 +13,19 @@ namespace snake
         static void Main(string[] args)
         {
 
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            Point p1 = new snake.Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 6;
-            char sym2 = '#';
-
-            Draw(x1, y1, sym1);
-            Draw(x2, y2, sym2);
+            Point p2 = new snake.Point();
+            p2.x = 4;
+            p2.y = 6;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
